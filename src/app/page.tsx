17334a67259c16abdd206f20f57fc2dc55d8c741
Rehmat778866 <rehmatkhalid11@gmail.com>
2024-08-23 23:08:58 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
       <h1 className="h-44 w-44 rounded-lg flex justify-center items-center text-2xl drop-shadow-md hover:drop-shadow-2xl bg-red-300  border-2 border-white hover:animate-bounce text-center">Hello World! My Name is Rehmat khalid</h1>
 
-      <h1 className="mt-8 text-md bg-red-700 text-white  p-2 rounded border-2 border-white">Congratulation! I am starting Next_Js</h1>
+      <h1 className="mt-3 text-md bg-red-700 text-white  p-2 rounded border-2 border-white">Congratulation! I am starting Next_Js</h1>
     </main>
   );
 }

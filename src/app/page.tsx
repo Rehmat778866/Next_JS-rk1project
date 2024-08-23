8a43174d78417da7dcb59a33ecce19e1ce72c0ef
Rehmat778866 <rehmatkhalid11@gmail.com>
2024-08-23 23:32:12 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-900">
+    <main className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-white lg:text-4xl md:text-2xl font-bold">Hello World!, My Name is Rehmat Khalid</h1>
+       
 
-      <h1 className="h-60 w-60 rounded-lg flex justify-center items-center text-2xl drop-shadow-md hover:drop-shadow-2xl bg-red-300  border-2 border-white hover:animate-bounce text-center">Hello World! My Name is Rehmat khalid</h1>
-
-      <h1 className="mt-2 text-md bg-white text-red-700  p-2 rounded border-2 border-red-700">Congratulation! I am starting Next_Js</h1>
     </main>
   );
 }

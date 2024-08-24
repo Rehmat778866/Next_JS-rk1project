f70e09e48c1b9b2d34239e1a6c6cf62a7f03eb9f
Rehmat778866 <rehmatkhalid11@gmail.com>
2024-08-24 09:18:31 +0500

    update link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-black">
       <h1 className="text-white lg:text-4xl md:text-2xl font-bold">Hello World!, My Name is Rehmat Khalid</h1>
+      <p className="text-md m-1 text-white"> Github Link: <a href="https://github.com/Rehmat778866/Next_JS-rk1project"></a></p>
        
 
     </main>
